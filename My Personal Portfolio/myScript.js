8435ece95b0277b85c703a93af688f673235e084
FarazFaruqi <faruqi.faraz@gmail.com>
2023-06-04 16:33:49 -0400

    personal-portfolio

---
+++ My Personal Portfolio/myScript.js
@@ -0,0 +1,9 @@
+button = document.getElementById("section_profile")
+console.log(button)
+button.style.color = "green";
+
+button.onclick = function() {
+  console.log("Button Clicked")
+  div_element = document.getElementById('button_response')
+  
+}
